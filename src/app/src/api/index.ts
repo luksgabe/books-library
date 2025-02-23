efb65cb0f2f9edc23f0e1974c7ad6ca5a836b9b8
Lucas Gabriel Pereira da Silva <llucasgabriel@hotmail.com>
2025-02-22 23:08:40 -0300

    Removed unecessary files

--- src/app/src/api/index.ts
+++ src/app/src/api/index.ts
@@ -1,29 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// src/api.ts
 import type { ApiResponse } from '@/types';
 import axios from 'axios';
 
-const BASE_URL = 'https://localhost:7208/api/book'; // Replace with your real API endpoint
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;;
 
-// Create an axios instance with a base URL
 const apiClient = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL as string,
     timeout: 5000,
 });
 
-// Function to fetch books using the search parameters
 export const fetchBooks = async (
     searchParam: string,
     page: number,
     size: number
 ): Promise<ApiResponse> => {
     try {
-        const response = await apiClient.get('', {
+        const response = await apiClient.get('/book', {
             params: { searchParam, page, size },
         });
         return response.data as ApiResponse;
     } catch (error: any) {
-        // You can enhance error handling as needed
         throw new Error(error.response?.data?.message || 'Error fetching books');
     }
 };
